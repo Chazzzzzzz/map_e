8745836f6a2b637852f861ec560b19d80cd787dc
Chaz <cuichangze@cuichangzes-MacBook-Pro.local>
2019-11-02 23:22:55 -0700

    finish bottom sheet update

--- app/src/main/java/com/example/map_e/GetNearbyPlaces.java
+++ app/src/main/java/com/example/map_e/GetNearbyPlaces.java
@@ -1,6 +1,7 @@
 package com.example.map_e;
 
 import android.os.AsyncTask;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,6 +10,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -16,11 +18,22 @@ public class GetNearbyPlaces extends AsyncTask<Object, String, String> {
 
     private String googleplaceData, url;
     private GoogleMap mMap;
+    private List<HashMap<String, TextView>> mapOfTextview;
+    private int bottomSheetUpdateCount = 0;
+    private LatLng userLatLng;
+
+    // used to calculate distance between two latlng
+    private static double EARTH_RADIUS = 6378.137;
+    // suppose that the car run with the speed of 8.333 m/s,
+    // we use the speed to estimate how long will it to take to reach the charging station
+    private static double speed = 8.333;
 
     @Override
     protected String doInBackground(Object... objects) {
         mMap = (GoogleMap) objects[0];
         url = (String) objects[1];
+        mapOfTextview = (List<HashMap<String, TextView>>) objects[2];
+        userLatLng = (LatLng) objects[3];
 
         DownloadUrl downloadUrl = new DownloadUrl();
         try {
@@ -46,6 +59,7 @@ public class GetNearbyPlaces extends AsyncTask<Object, String, String> {
             HashMap<String, String> googleNearbyPlace = nearbyPlacesList.get(i);
             String nameOfPlace = googleNearbyPlace.get("place_name");
             String vicinity = googleNearbyPlace.get("vicinity");
+            String availability = googleNearbyPlace.get("availability");
             double lat = Double.parseDouble(googleNearbyPlace.get("lat"));
             double lng = Double.parseDouble(googleNearbyPlace.get("lng"));
 
@@ -56,6 +70,34 @@ public class GetNearbyPlaces extends AsyncTask<Object, String, String> {
             mMap.addMarker(markerOptions);
             mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
             mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
+
+            // update bottom sheet
+            if (bottomSheetUpdateCount < 3) {
+                mapOfTextview.get(bottomSheetUpdateCount).get("name_station").setText(nameOfPlace);
+                mapOfTextview.get(bottomSheetUpdateCount).get("address").setText(vicinity);
+                int minutes = (int) Math.round(getDistanceBetweenLatlng(latLng, userLatLng) / (speed * 60));
+                mapOfTextview.get(bottomSheetUpdateCount).get("time").setText(Integer.toString(minutes) + " minutes");
+                mapOfTextview.get(bottomSheetUpdateCount).get("availability").setText(availability);
+                bottomSheetUpdateCount++;
+            }
         }
+    }
+
+    private static double rad(double d) {
+        return d * Math.PI / 180.0;
+    }
+
+    private double getDistanceBetweenLatlng(LatLng latLng1, LatLng latLng2) {
+        double radLat1 = rad(latLng1.latitude);
+        double radLat2 = rad(latLng2.latitude);
+        double a = radLat1 - radLat2;
+        double b = rad(latLng1.longitude) - rad(latLng2.longitude);
+        double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2)
+                + Math.cos(radLat1) * Math.cos(radLat2)
+                * Math.pow(Math.sin(b / 2), 2)));
+        s = s * EARTH_RADIUS;
+        s = Math.round(s * 10000d) / 10000d;
+        s = s*1000;
+        return s;
     }
 }
--- app/src/main/java/com/example/map_e/MapsActivity.java
+++ app/src/main/java/com/example/map_e/MapsActivity.java
@@ -7,6 +7,7 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -21,7 +22,9 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -39,8 +42,13 @@ import com.google.android.gms.maps.model.Marker;
 
 import java.io.IOException;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import android.widget.Spinner;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import android.view.inputmethod.InputMethodManager;
+
 
 public class MapsActivity extends FragmentActivity implements
         OnMapReadyCallback,
@@ -61,6 +69,9 @@ public class MapsActivity extends FragmentActivity implements
     private double latitude, longitude;
     private int radius = 10000;
 
+    private BottomSheetBehavior sheetBehavior;
+    private LinearLayout bottom_sheet;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,11 +81,24 @@ public class MapsActivity extends FragmentActivity implements
             checkUserLocationPermission();
         }
 
+        // add UI car select spinner
         Spinner spinner = findViewById(R.id.spinner_car);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.car_type, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
+        // add bottom sheet
+        bottom_sheet = findViewById(R.id.bottom_sheet);
+        sheetBehavior = BottomSheetBehavior.from(bottom_sheet);
+        sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+        bottom_sheet.setOnClickListener(new LinearLayout.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
+
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -89,6 +113,22 @@ public class MapsActivity extends FragmentActivity implements
             buildGoogleApiClient();
             mMap.setMyLocationEnabled(true);
         }
+
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            }
+        });
+
+        googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+                return false;
+            }
+        });
     }
 
     public boolean checkUserLocationPermission() {
@@ -173,9 +213,6 @@ public class MapsActivity extends FragmentActivity implements
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
         }
-
-
-
     }
 
     // for search address
@@ -219,21 +256,66 @@ public class MapsActivity extends FragmentActivity implements
                 Toast.makeText(this, "please input address", Toast.LENGTH_SHORT).show();
             }
         }
+        hideShowKeyboard();
+    }
+
+    public void hideShowKeyboard() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        if (imm.isActive()) {
+            imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, InputMethodManager.HIDE_NOT_ALWAYS);
+        }
     }
 
     private void searchNearByChargingStation() {
 
-        Object transferData[] = new Object[2];
+        Object transferData[] = new Object[4];
         GetNearbyPlaces getNearbyPlaces = new GetNearbyPlaces();
 
         String searchContent = "electric vehicle charging point";
         String url = getUrl(latitude, longitude, searchContent);
+        List<HashMap<String, TextView>> mapOfTextview = getMapOfTextview();
+        LatLng latLng = new LatLng(latitude, longitude);
+
         transferData[0] = mMap;
         transferData[1] = url;
+        transferData[2] = mapOfTextview;
+        transferData[3] = latLng;
 
         getNearbyPlaces.execute(transferData);
         Toast.makeText(this, "searching nearby charging station", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "showing nearby charging station", Toast.LENGTH_SHORT).show();
+    }
+
+    private List<HashMap<String, TextView>> getMapOfTextview() {
+        List<HashMap<String, TextView>> mapOfTextview = new ArrayList<>();
+
+        HashMap<String, TextView> singleMapTextview1 = new HashMap<>();
+        HashMap<String, TextView> singleMapTextview2 = new HashMap<>();
+        HashMap<String, TextView> singleMapTextview3 = new HashMap<>();
+
+        singleMapTextview1.put("name_station", (TextView) findViewById(R.id.name_station1));
+        singleMapTextview1.put("address", (TextView) findViewById(R.id.address1));
+        singleMapTextview1.put("time", (TextView) findViewById(R.id.time1));
+        singleMapTextview1.put("availability", (TextView) findViewById(R.id.availability1));
+        singleMapTextview1.put("time", (TextView) findViewById(R.id.time1));
+
+        singleMapTextview2.put("name_station", (TextView) findViewById(R.id.name_station2));
+        singleMapTextview2.put("address", (TextView) findViewById(R.id.address2));
+        singleMapTextview2.put("time", (TextView) findViewById(R.id.time2));
+        singleMapTextview2.put("availability", (TextView) findViewById(R.id.availability2));
+        singleMapTextview2.put("time", (TextView) findViewById(R.id.time2));
+
+        singleMapTextview3.put("name_station", (TextView) findViewById(R.id.name_station3));
+        singleMapTextview3.put("address", (TextView) findViewById(R.id.address3));
+        singleMapTextview3.put("time", (TextView) findViewById(R.id.time3));
+        singleMapTextview3.put("availability", (TextView) findViewById(R.id.availability3));
+        singleMapTextview3.put("time", (TextView) findViewById(R.id.time3));
+
+        mapOfTextview.add(singleMapTextview1);
+        mapOfTextview.add(singleMapTextview2);
+        mapOfTextview.add(singleMapTextview3);
+
+        return mapOfTextview;
     }
 
     private String getUrl(double latitude, double longitude, String searchContent) {
